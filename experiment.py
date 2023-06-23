aff20996ae1aabe9ef614bd72613dcbe57f6ac43
Mark Rucker <rucker.mark@gmail.com>
2023-06-23 13:32:10 -0700

    Made some small bug fixes.

--- experiment.py
+++ experiment.py
@@ -1,6 +1,8 @@
 import math
 import torch
+import numpy as np
 import coba as cb
+from coba.encodings import InteractionsEncoder
 
 class CauchyRewardModel(torch.nn.Module):
     def __init__(self, numrff, sigma):
@@ -20,7 +22,7 @@ class CauchyRewardModel(torch.nn.Module):
         self.linear = torch.nn.Linear(in_features=numrff, out_features=1)
 
     def featurize(self,context,actions):
-        I = cb.InteractionsEncoder(['a','xa'])
+        I = InteractionsEncoder(['a','xa'])
         X = [ I.encode(x=x,a=a) for x,A in zip(context,actions) for a in A]
         return torch.tensor(X,dtype=torch.float32)
 
@@ -41,7 +43,7 @@ class PolyRewardModel(torch.nn.Module):
         return {"argmax":"Poly"}
 
     def featurize(self,context,actions):
-        I = cb.InteractionsEncoder(['a','xa'])
+        I = InteractionsEncoder(['a','xa'])
         X = [ I.encode(x=x,a=a) for x,A in zip(context,actions) for a in A]
         return torch.tensor(X,dtype=torch.float32)
 
@@ -212,6 +214,92 @@ class EpsLearner:
         if self.v == 1:
             self.scheduler.step()
 
+class LinUCBLearner:
+    """A contextual bandit learner that represents expected reward as a
+    linear function of context and action features. Exploration is carried
+    out according to upper confidence bound estimates.
+
+    This is an implementation of the Chu et al. (2011) LinUCB algorithm using the
+    `Sherman-Morrison formula`__ to iteratively calculate the inversion matrix. This
+    implementation's computational complexity is linear with respect to feature count.
+
+    Remarks:
+        The Sherman-Morrsion implementation used below is given in long form `here`__.
+
+    References:
+        Chu, Wei, Lihong Li, Lev Reyzin, and Robert Schapire. "Contextual bandits
+        with linear payoff functions." In Proceedings of the Fourteenth International
+        Conference on Artificial Intelligence and Statistics, pp. 208-214. JMLR Workshop
+        and Conference Proceedings, 2011.
+
+    __ https://en.wikipedia.org/wiki/Sherman%E2%80%93Morrison_formula
+    __ https://research.navigating-the-edge.net/assets/publications/linucb_alternate_formulation.pdf
+    """
+
+    def __init__(self, alpha: float = 1, features  = [1, 'a', 'ax']) -> None:
+        """Instantiate a LinUCBLearner.
+
+        Args:
+            alpha: This parameter controls the exploration rate of the algorithm. A value of 0 will cause actions
+                to be selected based on the current best point estimate (i.e., no exploration) while a value of inf
+                means that actions will be selected based solely on the bounds of the action point estimates (i.e.,
+                we will always take actions that have the largest bound on their point estimate).
+            features: Feature set interactions to use when calculating action value estimates. Context features
+                are indicated by x's while action features are indicated by a's. For example, xaa means to cross the
+                features between context and actions and actions.
+        """
+
+        self._alpha = alpha
+
+        self._X = features
+        self._X_encoder = InteractionsEncoder(features)
+
+        self._theta = None
+        self._A_inv = None
+
+    @property
+    def params(self):
+        return {'family': 'LinUCB', 'alpha': self._alpha, 'features': self._X}
+
+    def predict(self, context, actions):
+
+        if not context:
+            self._X_encoder = InteractionsEncoder(list(set(filter(None,[ f.replace('x','') if isinstance(f,str) else f for f in self._X ]))))
+
+        context = context or []
+        features = np.array([self._X_encoder.encode(x=context,a=action) for action in actions]).T
+
+        if(self._A_inv is None):
+            self._theta = np.zeros(features.shape[0])
+            self._A_inv = np.identity(features.shape[0])
+
+        point_estimate = self._theta @ features
+        point_bounds   = np.diagonal(features.T @ self._A_inv @ features)
+
+        action_values = point_estimate + self._alpha*np.sqrt(point_bounds)
+        max_indexes   = np.where(action_values == np.amax(action_values))[0]
+
+        return [int(ind in max_indexes)/len(max_indexes) for ind in range(len(actions))]
+
+    def learn(self, context, actions, action, reward: float, probability: float) -> None:
+
+        if not context:
+            self._X_encoder = InteractionsEncoder(list(set(filter(None,[ f.replace('x','') if isinstance(f,str) else f for f in self._X ]))))
+
+        context = context or []
+        features = np.array(self._X_encoder.encode(x=context,a=action)).T
+
+        if(self._A_inv is None):
+            self._theta = np.zeros((features.shape[0]))
+            self._A_inv = np.identity(features.shape[0])
+
+        r = self._theta @ features
+        w = self._A_inv @ features
+        v = features    @ w
+
+        self._A_inv = self._A_inv - np.outer(w,w)/(1+v)
+        self._theta = self._theta + (reward-r)/(1+v) * w
+
 torch.set_num_threads(1)
 
 if __name__ == "__main__":
@@ -220,12 +308,12 @@ if __name__ == "__main__":
     lr, tz = 0.02, 100
 
     env = cb.Environments.cache_dir('.coba_cache').from_template('./208_multiclass.json',n_take=10_000).where(n_interactions=10_000)
-    env = env[:50].scale('min','minmax')
-    env = (env.cycle(0) + env.cycle(6000)).batch(8)
+    env = env[:50].scale('min','minmax') #filter down to a smaller batch of datasets so it completes sooner
+    env = (env.cycle(0) + env.cycle(6000)).batch(8) #mini batches of size 8
 
     lrn = [
         IGWLearner(CauchyRewardModel(2048, .08),"lambda t: 0 + 10*t**(3/4)",lr/2,tz,False,2,2),
-        cb.LinUCBLearner(features=['a','ax']),
+        LinUCBLearner(features=['a','ax']),
         EpsLearner(CauchyRewardModel(2048, .08),.05,lr/2,tz,False,2,2)
     ]
 
